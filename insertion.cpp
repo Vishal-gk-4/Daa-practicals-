78ba3bfe43bf6576f3989a76aa3d534fd02a3e02
Vishal Gk <vishalgk0000@gmail.com>
2026-07-31 13:03:02 +0530

    Refactor insertion sort implementation

--- insertion.cpp
+++ insertion.cpp
@@ -6,19 +6,18 @@ int main()
     int arr[] = {5, 3, 8, 4, 2};
     int n = 5;
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 1; i < n; i++)
     {
-        int minIndex = i;
+        int key = arr[i];
+        int j = i - 1;
 
-        for (int j = i + 1; j < n; j++)
+        while (j >= 0 && arr[j] > key)
         {
-            if (arr[j] < arr[minIndex])
-            {
-                minIndex = j;
-            }
+            arr[j + 1] = arr[j];
+            j--;
         }
 
-        swap(arr[i], arr[minIndex]);
+        arr[j + 1] = key;
     }
 
     cout << "Sorted array: ";
